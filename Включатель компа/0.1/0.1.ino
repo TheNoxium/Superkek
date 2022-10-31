8f8a1332c121a13591e8f93dab00a1ef97f199e0
TheNoxium <113540766+TheNoxium@users.noreply.github.com>
2022-11-01 02:25:43 +0500

    Update 0.1.ino

--- Включатель компа/0.1/0.1.ino
+++ Включатель компа/0.1/0.1.ino
@@ -1,16 +1,51 @@
+
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <TelegramBot.h>
 
-#define OPT 5 // D1
+#define OPT 2 // D1
 
+const char* ssid = "Cats_Ass"
+const char* pas = "12345678s"
+const char* Bitt = "1617873195:AAGpNItFduM1HgtZeajYfM0s9ZsMAP-jLaI"
+
+WiFiClientSecure net_ssl; 
+TelegramBot bot (Bitt, net_ssl);
 
 void setup() {
-  // put your setup code here, to run once:
 
+   Serial.begin (9600);
+   Serial.println(Jopa);
+   
+   WiFi.begin(ssid, pas);
+     Serial.print("Connecting");
+     
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Bot.begin();
+
+  pinMode(OTP, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  message m = bot.getUodates();
+
+  if (m.text.equals("Я пидор")){
+    digitalWrite(OTP, HIGH);
+    bot.sendMessage(Что правда то правда);
+
+  }else if (m.text.equals("off")){
+    digitalWrite(OTP, LOW);
+     bot.sendMessage(и кстати пошел нахуй);
+  }
+}
 
 }
