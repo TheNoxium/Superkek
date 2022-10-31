f79345744a57f6d3114d1f384a6bc9e66af0d5a7
TheNoxium <113540766+TheNoxium@users.noreply.github.com>
2022-11-01 01:11:12 +0500

    Пин
    Добавил пин для оптрона

---
+++ Включатель компа/0.1/0.1.ino
@@ -0,0 +1,16 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <TelegramBot.h>
+
+#define OPT 5 // D1
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
