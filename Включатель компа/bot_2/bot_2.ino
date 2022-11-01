e0d7982c1311eeaca8c47e8ebbf990906fa5502a
TheNoxium <113540766+TheNoxium@users.noreply.github.com>
2022-11-02 04:16:23 +0500

    Update bot_2.ino

--- Включатель компа/bot_2/bot_2.ino
+++ Включатель компа/bot_2/bot_2.ino
@@ -1,11 +1,17 @@
 #define WIFI_SSID "Cats_Ass"
 #define WIFI_PASS "12345678s"
 #define BOT_TOKEN "1617873195:AAGpNItFduM1HgtZeajYfM0s9ZsMAP-jLaI"
+//#define 2 "LED_BUILTIN"
+
 #include <FastBot.h>
+
 FastBot bot(BOT_TOKEN);
 void setup() {
   connectWiFi();
+  bot.setChatID("-847090362, 1488559189");
+  bot.sendMessage("Здарова пидарас, чо опять решил в свой копуктер играть из своей турции, лучше бы ты сосал хуи как профессиональная шлюха чем лез сюда, Щенок!");
   bot.attach(newMsg);
+  pinMode(D0, OUTPUT);
 }
 // обработчик сообщений
 void newMsg(FB_msg& msg) {
@@ -15,7 +21,21 @@ void newMsg(FB_msg& msg) {
   Serial.print(msg.username);   // логин
   Serial.print(", ");
   Serial.println(msg.text);     // текст
-  bot.sendMessage(msg.text, msg.chatID);  
+ // bot.sendMessage(msg.text, msg.chatID);  
+ if (msg.text == "Я уже грязная шлюха") bot.sendMessage("Да сучка это так!", msg.chatID);
+ delay(1000);
+ if (msg.text == "Я уже грязная шлюха") bot.sendMessage("Продолжай говорить что ты дрянь", msg.chatID);
+ delay(2000);
+if (msg.text == "Я уже грязная шлюха") bot.sendMessage("Меня это пиздец как заводит", msg.chatID); 
+delay(3000);
+if (msg.text == "Я уже грязная шлюха") bot.sendMessage("Хочешь чтобы я включил твой ПК, падаль?", msg.chatID); 
+
+if (msg.text == "Да") bot.sendMessage("Скажи мне что ты пидорок!", msg.chatID);
+if (msg.text == "Я пидорок") bot.sendMessage("Ну ладно я включаю твою разьебайку!", msg.chatID);
+if (msg.text == "Я пидорок") digitalWrite(D0, HIGH );
+delay(200);
+digitalWrite(D0, LOW);
+
 }
   
 
